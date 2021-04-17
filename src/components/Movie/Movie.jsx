6575dd124e8d89c250bf7dc55367166a4665cd04
facureyes <facundotomasreyes@gmail.com>
2021-04-17 15:15:17 -0300

    Changed the style of the movie cards

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,35 +1,22 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './Movie.css'
 
 const Movie = (props) => {
     return (
-        <div className="Movie">
-            
-            {/* <div class="card" style={{'backgroundImage':`url(${"https://image.tmdb.org/t/p/w342/" + props.movie.poster_path})`}}>
-                <div class="content" >
-                    <h2 class="title">{props.movie.title}</h2>
-                    <p class="copy">{`Rating: ${props.movie.vote_average}/10`}</p>
-                    <button class="btn">Details</button>
-                </div>
-            </div> */}
-
-            {/* <div class="box" style={{'backgroundImage':`url(${"https://image.tmdb.org/t/p/w342/" + props.movie.poster_path})`}}>
-                <h1>{props.movie.title}</h1>
-
-            </div> */}
-
-
-
-
-            <Card bg='dark' text='white' style={{'margin':'1rem 0'}}>
+        <div className="Movie" onClick={()=>{alert(props.movie.vote_average)}}>
+            <Card className="bg-dark text-white border-0" >
                 <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w342/" + props.movie.poster_path} />
-                <Card.Body>
-                    <Card.Title style={{'height':'4rem'}}>{props.movie.title}</Card.Title>
-                    <Button variant="outline-light">Details</Button>
-                </Card.Body>
+                <div className="card-title-background"></div>
+                <div className="card-title">
+                    <h5>{props.movie.title}</h5>
+                </div>
+                <div className="card-button">
+                    <h5>+ Details</h5>
+                </div>
             </Card>
+
+            
         </div>
     );
 }
