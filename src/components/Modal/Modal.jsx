3b8ae2efc5e1d8652ecf0e9cd3b21e14e286bd68
facureyes <facundotomasreyes@gmail.com>
2021-04-18 16:09:39 -0300

    Adapted modal style to mobile phones

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,18 +3,49 @@ import { createPortal } from "react-dom";
 import './Modal.css'
 
 const Modal = props => {
+
+    const convertMinsToHrsMins = (mins) => {
+        let h = Math.floor(mins / 60);
+        let m = mins % 60;
+        m = m < 10 ? '0' + m : m;
+        return `${h}:${m}`;
+    }
+
     return ( 
         props.visible ? createPortal(
             <div className="Modal" onClick={props.close}>
                 <div className="modal-box" style={{'backgroundImage':`url(https://image.tmdb.org/t/p/w1280${props.movie.backdrop_path})`}}>
                     <div className="modal-background"></div>
                     <div className="modal-content">
-                        <h1>{props.movie.title}</h1>
-                        {props.movie.release_date.search(/\d{4}/) !== -1?
-                            <h6 className="modal-info">{props.movie.release_date.match(/\d{4}/)[0]}</h6>
-                            : null}
-                        <h6 className="modal-info">{props.movie.vote_average}</h6>
+                        <h1 className="modal-title">{props.movie.title.toUpperCase()}</h1>
+                        <div className="modal-genres">
+                            {props.movie.genres.map(el => {
+                                return <h6 className="modal-genre">{el.name}</h6>
+                            })}
+                        </div>
+
+                        <div className="modal-specs">
+                            {props.movie.release_date.search(/\d{4}/) !== -1?
+                                <h6 className="modal-info">
+                                    <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                                    {" " + props.movie.release_date.match(/\d{4}/)[0]}
+                                </h6>
+                                : null}
+                            <h6 className="modal-info">
+                                <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                {" " + convertMinsToHrsMins(parseInt(props.movie.runtime)) }
+                            </h6>
+                            <h6 className="modal-info">
+                                <i class="fa fa-star-o" aria-hidden="true"></i>
+                                {" " + props.movie.vote_average}
+                            </h6>
+                            
+                        </div>
+                        
+                        <h4>{props.movie.tagline}</h4>
                         <p>{props.movie.overview}</p>
+                        
+                        
                     </div>
                     
                 </div>
