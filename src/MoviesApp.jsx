818d46b82b7cbfe2344f69b95cdb1eb0321b8a42
facureyes <facundotomasreyes@gmail.com>
2021-04-18 16:13:00 -0300

    Added Modal component and functionalityt

--- src/MoviesApp.jsx
+++ src/MoviesApp.jsx
@@ -3,19 +3,26 @@ import './MoviesApp.css';
 import MovieGrid from './components/MovieGrid/MovieGrid';
 import NavBar from './components/NavBar/NavBar'
 import RatingFilter from './components/RatingFilter/RatingFilter'
+import Modal from './components/Modal/Modal';
 import axios from 'axios';
 import Aux from './hoc/AuxHoc';
 
 function MoviesApp() {
   const [rating, setRating] = useState(0);
-  // const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
+  const [modal, setModal] = useState({
+    visible: false,
+    data: {}
+  });
+  // const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     handleMoviesSearch();
   }, []);
 
-  // Need to protect API Key before uploading
+  // If this page will be running on a server I will need to add a configure call to the api to
+  // get escential things such as base URL for images and images sizes instead of hardcoding it
+  // Need to protect API Key before uploading - Not doing so in this prototype
   const handleMoviesSearch = input => {
     let query;
     if(input === undefined || input === ""){
@@ -45,11 +52,49 @@ function MoviesApp() {
     });
   };
 
+  const openModal = mov => {
+    let query = `https://api.themoviedb.org/3/movie/${mov.id}?api_key=8fa4d18f25de3243e9c147cf34f534f2&language=en-US`;
+    
+    axios.get(query).then(res => {
+      if (res.request.status === 200){ 
+        const newModal = {
+          visible: true,
+          data: {...res.data}
+        };
+        console.log(res);
+        setModal(newModal);
+      } else {
+        const newModal = {
+          visible: true,
+          data: {...mov}
+        };
+        setModal(newModal);
+      }
+    });
+  }
+
+const closeModal = () => {
+  setModal(prevStatus => ({...prevStatus, visible: false}));
+}
+
   return (
     <Aux>
       <NavBar search={handleMoviesSearch}/>
+      <Modal
+        visible={modal.visible}
+        close={closeModal}
+        effect="fadeInUp"
+        movie={modal.data}
+      >
+      {/* <h1>Title</h1> */}
+        {/* <div>
+            <h1>{modal.data.title}</h1>
+            <p>Some Contents</p>
+            <a href="javascript:void(0);" onClick={() => closeModal()}>Close</a>
+        </div> */}
+      </Modal>
       <RatingFilter clicked={handleRating} rating={rating}/>
-      <MovieGrid movies={movies} rating={rating} />
+      <MovieGrid movies={movies} rating={rating} onClick={openModal}/>
       {/* <Footer /> */}
     </Aux>
     
